62d6d52c5f305c6d1b45b3dd19264e7db9d94095
Yuga-Shibata <kazutodaze3@gmail.com>
2020-06-05 12:53:43 +0900

    animationを追加

---
+++ Swift5QuitDrinking/QuitViewController.swift
@@ -0,0 +1,20 @@
+//
+//  QuitViewController.swift
+//  Swift5QuitDrinking
+//
+//  Created by 柴田優河 on 2020/06/05.
+//  Copyright © 2020 Makoto Shibata. All rights reserved.
+//
+
+import UIKit
+
+class QuitViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+    
+
+}
--- Swift5QuitDrinking/ViewController.swift
+++ Swift5QuitDrinking/ViewController.swift
@@ -20,9 +20,14 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         
         // animationを実行
-        animation()
+//        animation()
     }
     @IBAction func start(_ sender: Any) {
+        // 画面遷移をする
+        //performSegue(withIdentifier: "next", sender: nil)
+        let nextVC = storyboard?.instantiateViewController(identifier: "quit") as! QuitViewController
+        navigationController?
+            .pushViewController(nextVC, animated: true)
         
     }
     
@@ -32,7 +37,7 @@ class ViewController: UIViewController {
 
     func animation(){
         let animation = Animation.named("beer")
-        animationView.frame = CGRect(x: 0, y: 0, width: view.frame.size.width, height: view.frame.size.height)
+        animationView.frame = CGRect(x: 0, y: (view.frame.size.height / 5) * -1, width: view.frame.size.width, height: view.frame.size.height)
         animationView.animation = animation
         animationView.contentMode = .scaleAspectFit
         // 一度だけ、アニメーションを実行
